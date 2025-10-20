ca6ef89418199c3c686a65bd7f56ace97d453504
Stuart Reid <sturei189@gmail.com>
2025-10-20 09:29:09 +0100

    used ArcBallCamera example in Hello Magnum

--- src/main/helloMagnum_main.cpp
+++ src/main/helloMagnum_main.cpp
@@ -1,23 +1,48 @@
 #include <Corrade/Containers/StringView.h>
+
+#include <Magnum/DebugTools/ColorMap.h>
+
 #include <Magnum/GL/Context.h>
 #include <Magnum/GL/DefaultFramebuffer.h>
 #include <Magnum/GL/Mesh.h>
 #include <Magnum/GL/Renderer.h>
 #include <Magnum/GL/Version.h>
+
+
+#include <Magnum/ImageView.h>
+#include <Magnum/PixelFormat.h>
+#include <Magnum/GL/Texture.h>
+#include <Magnum/GL/TextureFormat.h>
+
+#include <Magnum/SceneGraph/Camera.h>
+#include <Magnum/SceneGraph/Drawable.h>
+#include <Magnum/SceneGraph/MatrixTransformation3D.h>
+#include <Magnum/SceneGraph/Object.h>
+#include <Magnum/SceneGraph/Scene.h>
 #include <Magnum/Math/Angle.h>
 #include <Magnum/Math/Color.h>
+#include <Magnum/Math/Time.h>
 #include <Magnum/Math/Matrix4.h>
 #include <Magnum/MeshTools/Compile.h>
 #include <Magnum/Platform/Sdl2Application.h>
 #include <Magnum/Primitives/Cube.h>
 #include <Magnum/Shaders/PhongGL.h>
 #include <Magnum/Shaders/VertexColorGL.h>
+#include <Magnum/Shaders/MeshVisualizerGL.h>
 #include <Magnum/Trade/MeshData.h>
 
-using namespace Magnum::Math::Literals;
+#include "gui/helloMagnum/ArcBall.h"
+#include "gui/helloMagnum/ArcBallCamera.h"
+
+using namespace Magnum;
+using Object3D = SceneGraph::Object<SceneGraph::MatrixTransformation3D>;
+using Scene3D = SceneGraph::Scene<SceneGraph::MatrixTransformation3D>;
+using namespace Math::Literals;
+
 
 // Implementation notes:
 // Except for Magnum's user-defined literals, chose to be explicit about Magnum namespaces to avoid confusion.
+// Probably a better idea is to put the Magnum Application class in its own file, and then use 'using namespace Magnum' inside that file.  
 // Switched Intellisense to use the tag parser. The new parser doesn't work very well with namespaces.
 // TODO: remove using declarations from header files (once headers and code are more stable and separated)
 
@@ -27,8 +52,12 @@ class MyApplication: public Magnum::Platform::Sdl2Application {
 
     private:
         void drawEvent() override;
+        void viewportEvent(ViewportEvent& event) override;
+        void keyPressEvent(KeyEvent& event) override;
+        void pointerPressEvent(PointerEvent& event) override;
         void pointerReleaseEvent(PointerEvent& event) override;
         void pointerMoveEvent(PointerMoveEvent& event) override;
+        void scrollEvent(ScrollEvent& event) override;
 
         Magnum::GL::Mesh _triangleMesh;
 
@@ -38,7 +67,39 @@ class MyApplication: public Magnum::Platform::Sdl2Application {
         Magnum::GL::Mesh _cubeMesh;
         Magnum::Matrix4 _cubeTransformation, _cubeProjection;
         Magnum::Color3 _cubeColor;
+
+        Scene3D _scene;
+        Magnum::SceneGraph::DrawableGroup3D _drawables;
+        Containers::Optional<Magnum::Examples::ArcBallCamera> _arcballCamera;
+        GL::Mesh _arcballMesh{NoCreate};
+
+        /* Stuff for visualizing the cube in the arcball example*/
+        Shaders::MeshVisualizerGL3D _arcballShader{NoCreate};
+        GL::Texture2D _arcballColormap{NoCreate};
+
 };
+
+class VisualizationDrawable: public SceneGraph::Drawable3D {
+    public:
+        explicit VisualizationDrawable(Object3D& object,
+            Shaders::MeshVisualizerGL3D& shader, GL::Mesh& mesh,
+            SceneGraph::DrawableGroup3D& drawables):
+                SceneGraph::Drawable3D{object, &drawables}, _shader(shader),
+                _mesh(mesh) {}
+
+        void draw(const Matrix4& transformation, SceneGraph::Camera3D& camera) {
+            _shader
+                .setTransformationMatrix(transformation)
+                .setProjectionMatrix(camera.projectionMatrix())
+                .draw(_mesh);
+        }
+
+    private:
+        Shaders::MeshVisualizerGL3D& _shader;
+        GL::Mesh& _mesh;
+};
+
+
 
 MyApplication::MyApplication(const Arguments& arguments): Sdl2Application(arguments, 
     Configuration{}.setTitle("Hello Magnum!")) {
@@ -84,14 +145,61 @@ MyApplication::MyApplication(const Arguments& arguments): Sdl2Application(argume
         Magnum::Matrix4::translation(Magnum::Vector3::zAxis(-10.0f));
     _cubeColor = Magnum::Color3::fromHsv({35.0_degf, 1.0f, 1.0f});
 
+    /* Setup a cube with vertex ID and wireframe visualized */
+    {
+        const Trade::MeshData cube = Primitives::cubeSolid();
+        _arcballMesh = MeshTools::compile(cube);
+
+        const auto map = DebugTools::ColorMap::turbo();
+        const Vector2i size{Int(map.size()), 1};
+        _arcballColormap = GL::Texture2D{};
+        _arcballColormap
+            .setMinificationFilter(SamplerFilter::Linear)
+            .setMagnificationFilter(SamplerFilter::Linear)
+            .setWrapping(SamplerWrapping::ClampToEdge)
+            .setStorage(1, GL::TextureFormat::RGB8, size)
+            .setSubImage(0, {}, ImageView2D{PixelFormat::RGB8Unorm, size, map});
+
+        _arcballShader = Shaders::MeshVisualizerGL3D{Shaders::MeshVisualizerGL3D::Configuration{}
+            .setFlags(Shaders::MeshVisualizerGL3D::Flag::Wireframe|
+                      Shaders::MeshVisualizerGL3D::Flag::VertexId)};
+        _arcballShader
+            .setViewportSize(Vector2{framebufferSize()})
+            .setColor(0xffffff_rgbf)
+            .setWireframeColor(0xffffff_rgbf)
+            .setWireframeWidth(2.0f)
+            .setColorMapTransformation(0.0f, 1.0f/cube.vertexCount())
+            .bindColorMapTexture(_arcballColormap);
+
+        auto object = new Object3D{&_scene};
+        (*object)
+            .rotateY(40.0_degf)
+            .rotateX(-30.0_degf)
+            ;
+        new VisualizationDrawable{*object, _arcballShader, _arcballMesh, _drawables};
+    }
+
+    /* Set up the camera */
+    /* Setup the arcball after the camera objects */
+    const Vector3 eye = Vector3::zAxis(-10.0f);
+    const Vector3 center{};
+    const Vector3 up = Vector3::yAxis();
+    _arcballCamera.emplace(_scene, eye, center, up, 45.0_degf,
+        windowSize(), framebufferSize());
+
+    /* Loop at 60 Hz max */
+    setSwapInterval(1);
+    setMinimalLoopPeriod(16.0_msec);
+
+
 }
 
 void MyApplication::drawEvent() {
     Magnum::GL::defaultFramebuffer.clear(Magnum::GL::FramebufferClear::Color|Magnum::GL::FramebufferClear::Depth);
 
-     _flatShader.draw(_triangleMesh);
+    _flatShader.draw(_triangleMesh);
 
-     _phongShader.setLightPositions({{1.4f, 1.0f, 0.75f, 0.0f}})
+    _phongShader.setLightPositions({{1.4f, 1.0f, 0.75f, 0.0f}})
         .setDiffuseColor(_cubeColor)
         .setAmbientColor(Magnum::Color3::fromHsv({_cubeColor.hue(), 1.0f, 0.3f}))
         .setTransformationMatrix(_cubeTransformation)
@@ -99,35 +207,118 @@ void MyApplication::drawEvent() {
         .setProjectionMatrix(_cubeProjection)
         .draw(_cubeMesh);
 
+    /* Call arcball update in every frame. This will do nothing if the camera
+       has not been changed. Otherwise, camera transformation will be
+       propagated into the camera objects. */
+    bool camChanged = _arcballCamera->update();
+    _arcballCamera->draw(_drawables);
     swapBuffers();
+
+    if(camChanged) redraw();
+}
+
+void MyApplication::viewportEvent(ViewportEvent& event) {
+    Magnum::GL::defaultFramebuffer.setViewport({{}, event.framebufferSize()});
+
+    _arcballCamera->reshape(event.windowSize(), event.framebufferSize());
+}
+
+void MyApplication::keyPressEvent(KeyEvent& event) {
+    switch(event.key()) {
+        case Key::L:
+            if(_arcballCamera->lagging() > 0.0f) {
+                Debug{} << "Lagging disabled";
+                _arcballCamera->setLagging(0.0f);
+            } else {
+                Debug{} << "Lagging enabled";
+                _arcballCamera->setLagging(0.85f);
+            }
+            break;
+        case Key::R:
+            _arcballCamera->reset();
+            break;
+
+        default: return;
+    }
+
+    event.setAccepted();
+    redraw(); /* camera has changed, redraw! */
+}
+
+void MyApplication::pointerPressEvent(PointerEvent& event) {
+    if(!event.isPrimary() ||
+       !(event.pointer() & (Pointer::MouseLeft|Pointer::Finger)))
+        return;
+
+    /* Enable mouse capture so the mouse can drag outside of the window */
+    /** @todo replace once https://github.com/mosra/magnum/pull/419 is in */
+    SDL_CaptureMouse(SDL_TRUE);
+
+    _arcballCamera->initTransformation(event.position());
+
+    event.setAccepted();
+    redraw(); /* camera has changed, redraw! */
 }
 
 void MyApplication::pointerReleaseEvent(Magnum::Platform::Sdl2Application::PointerEvent& event) {
     if(!event.isPrimary() ||
-       !(event.pointer() & (Magnum::Platform::Sdl2Application::Pointer::MouseLeft|Magnum::Platform::Sdl2Application::Pointer::Finger)))
+       !(event.pointer() & (Pointer::MouseLeft|Pointer::Finger)))
         return;
 
-    _cubeColor = Magnum::Color3::fromHsv({_cubeColor.hue() + 50.0_degf, 1.0f, 1.0f});
+    /* Disable mouse capture again */
+    /** @todo replace once https://github.com/mosra/magnum/pull/419 is in */
+    SDL_CaptureMouse(SDL_FALSE);
 
-    event.setAccepted();
-    redraw();
+
+    // From anpother example.
+    //if(!event.isPrimary() ||
+    //   !(event.pointer() & (Magnum::Platform::Sdl2Application::Pointer::MouseLeft|Magnum::Platform::Sdl2Application::Pointer::Finger)))
+    //    return;
+    //
+    //_cubeColor = Magnum::Color3::fromHsv({_cubeColor.hue() + 50.0_degf, 1.0f, 1.0f});
+
+    //event.setAccepted();
+    //redraw();
 }
 
 void MyApplication::pointerMoveEvent(Magnum::Platform::Sdl2Application::PointerMoveEvent& event) {
     if(!event.isPrimary() ||
-       !(event.pointers() & (Magnum::Platform::Sdl2Application::Pointer::MouseLeft|Magnum::Platform::Sdl2Application::Pointer::Finger)))
+       !(event.pointers() & (Pointer::MouseLeft|Pointer::Finger)))
         return;
 
-    Magnum::Vector2 delta = 3.0f*Magnum::Vector2{event.relativePosition()}/Magnum::Vector2{windowSize()};
-
-    _cubeTransformation =
-        Magnum::Matrix4::rotationX(Magnum::Rad{delta.y()})*
-        _cubeTransformation*
-        Magnum::Matrix4::rotationY(Magnum::Rad{delta.x()});
+    if(event.modifiers() & Modifier::Shift)
+        _arcballCamera->translate(event.position());
+    else _arcballCamera->rotate(event.position());
 
     event.setAccepted();
-    redraw();
+    redraw(); /* camera has changed, redraw! */
+
+    // from another example
+    //if(!event.isPrimary() ||
+    //   !(event.pointers() & (Magnum::Platform::Sdl2Application::Pointer::MouseLeft|Magnum::Platform::Sdl2Application::Pointer::Finger)))
+    //    return;
+    //
+    //Magnum::Vector2 delta = 3.0f*Magnum::Vector2{event.relativePosition()}/Magnum::Vector2{windowSize()};
+
+    //_cubeTransformation =
+    //    Magnum::Matrix4::rotationX(Magnum::Rad{delta.y()})*
+    //    _cubeTransformation*
+    //    Magnum::Matrix4::rotationY(Magnum::Rad{delta.x()});
+
+    //event.setAccepted();
+    //redraw();
 }
+
+void MyApplication::scrollEvent(ScrollEvent& event) {
+    const Float delta = event.offset().y();
+    if(Math::abs(delta) < 1.0e-2f) return;
+
+    _arcballCamera->zoom(delta);
+
+    event.setAccepted();
+    redraw(); /* camera has changed, redraw! */
+}
+
 
 //MAGNUM_SDL2APPLICATION_MAIN(MyApplication) // Using my own main function instead of this macro for transparency
 int main(int argc, char** argv) {
