20c98b679f2aefe3451daf7613642b81cb962dff
Stuart Reid <sturei189@gmail.com>
2025-10-28 23:26:10 +0000

    a bit more on testing with jest-dom: import the types to avoid the squiggles

--- web/e2_viewer/src/components/Drawlist.test.tsx
+++ web/e2_viewer/src/components/Drawlist.test.tsx
@@ -2,6 +2,7 @@ import { describe, it, expect, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { Drawlist } from './Drawlist';
 import type { IDrawable } from './Drawable';
+import "@testing-library/jest-dom";
 
 /**
  * @vitest-environment jsdom
