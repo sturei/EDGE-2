856a8227d6972c742b91de00276073e3c65a1f86
Stuart Reid <sturei189@gmail.com>
2025-12-17 10:18:55 +0000

    delete all the low-level actions that were needed originally for testing, but are not needed now

--- engines/include/shape/shape.actions.h
+++ engines/include/shape/shape.actions.h
@@ -5,23 +5,11 @@
 
 namespace e2 {
     namespace ShapeActions {
-        void addEmptyBody(Document& doc, const nlohmann::json& payload);
-        void addAcornBody(Document& doc, const nlohmann::json& payload);
-        void addWireRectangle(Document& doc, const nlohmann::json& payload);
-        void addWireRoundRect(Document& doc, const nlohmann::json& payload);
-        void addSheetRectangle(Document& doc, const nlohmann::json& payload);
-        void addSheetRoundRect(Document& doc, const nlohmann::json& payload);
         void addPrimitive2D(Document& doc, const nlohmann::json& payload);
         void addPrimitive(Document& doc, const nlohmann::json& payload);
         void addExtrusion(Document& doc, const nlohmann::json& payload);
 
-        inline static std::array<ActionDef, 16> allShapeActions = {{
-            {"Sketches::addEmptyBody", addEmptyBody},
-            {"Sketches::addAcornBody", addAcornBody},
-            {"Sketches::addRectangle", addWireRectangle},
-            {"Sketches::addRoundRect", addWireRoundRect},
-            {"Profiles::addRectangle", addSheetRectangle},
-            {"Profiles::addRoundRect", addSheetRoundRect},
+        inline static std::array<ActionDef, 3> allShapeActions = {{
             {"Profiles::addPrimitive", addPrimitive2D},
             {"Features::addPrimitive", addPrimitive},
             {"Features::addExtrusion", addExtrusion}
